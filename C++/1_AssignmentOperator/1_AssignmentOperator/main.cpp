c4c8f2c22cbeb87620de1b455e0bf6f37f64299c
Kris_Chan <kris.dacpc@gmail.com>
2017-07-17 14:54:30 +0800

    1.add the C++ 01;

---
+++ C++/1_AssignmentOperator/1_AssignmentOperator/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  1_AssignmentOperator
+//
+//  Created by Kris on 17/07/2017.
+//  Copyright © 2017 Kris. All rights reserved.
+//
+
+/*
+Problem：赋值运算符函数
+Desc：如下为类型CMyString的声明，请为该类型添加赋值运算符函数。
+
+class CMyString{
+public:
+    CMyString(char* pData = nullptr);
+    CMyString(const CMyString& str);
+    ~CMyString(void);
+    
+private:
+    char* m_pData;
+};
+*/
+
