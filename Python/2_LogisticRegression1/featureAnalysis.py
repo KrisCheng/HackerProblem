9bd347b2a5f25af8cd4a0f9cb04e4b0cd749bd15
Kris_Chan <kris.dacpc@gmail.com>
2017-10-26 23:26:27 +0800

    1.init the file

---
+++ Python/2_LogisticRegression1/featureAnalysis.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Desc: the analysis file of the project.
+DataSet: Connectionist Bench (Sonar, Mines vs. Rocks) Data Set 
+Author: Kris Peng
+Copyright (c) 2017 - Kris Peng <kris.dacpc@gmail.com>
+'''
+
+print("Hello")
