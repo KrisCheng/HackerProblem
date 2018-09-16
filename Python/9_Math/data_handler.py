099087869ba71080327289d15225fae734a8d380
Kris_Chan <kris.dacpc@gmail.com>
2018-09-16 11:52:06 +0800

    1,修改FIFO 算法 bug;

--- Python/9_Math/data_handler.py
+++ Python/9_Math/data_handler.py
@@ -1,7 +1,8 @@
-import os
 from datetime import datetime
 import dateutil.parser
 from openpyxl import load_workbook
+from matplotlib import pyplot
+import pylab as plt
 
 WIDE_BODY = ["332", "333", "33E", "33H", "33L", "773"]
 NARROW_BODY = ["319", "320", "321", "323", "325", "738", "73A", "73E", "73H", "73L"]
@@ -16,8 +17,8 @@ def __time_transfer(begin_time, end_time, priority):
         begin.pop()
     if(len(end) == 3):
         end.pop()
-    begin_index = int(begin[0].strip())*12 + int(int(begin[1].strip())/5)
-    end_index = int(end[0].strip())*12 + int(int(end[1].strip())/5)
+    begin_index = int(int(begin[0].strip())*12) + int(int(begin[1].strip())/5)
+    end_index = int(int(end[0].strip())*12) + int(int(end[1].strip())/5)
     if priority == 1:
         begin_index = 0
     elif priority == 3:
@@ -55,6 +56,7 @@ def __load_data_sources():
                 if isinstance(cell_value, datetime):
                     cell_value = cell_value.strftime("%Y-%m-%d")
                 dict[cell_key] = str(cell_value)
+        # 筛除不需要考虑的航班
         if not(dict["到达日期"] != "2018-01-20" and dict["出发日期"] != "2018-01-20"):
             list_pucks.append(dict)
 
@@ -85,7 +87,6 @@ def __load_data_sources():
 
 def main_task():
     list_pucks, list_tickets, list_gates = __load_data_sources()
-
     # 1. 将所有需要考虑的航班记录 303*2 = 606 [id, type, date, time, plane_type, D/I, W/N]
     list_airline = []
     for puck in list_pucks:
@@ -119,9 +120,8 @@ def main_task():
             print("No Such Plane!!!")
         list_airline.append(launch_airline)
     
-    # 2. 特殊条件处理 “每架飞机转场的到达和出发两个航班必须分配在同一登机口进行，其间不能挪移别处；”
+    # 2. 优先级处理 考虑--“每架飞机转场的到达和出发两个航班必须分配在同一登机口进行，其间不能挪移别处；”
     for puck in list_pucks:
-        
     # 飞机分类
     # 19号到达，20号起飞 优先级 1
     # 20号到达，20号起飞 优先级 2
@@ -129,7 +129,7 @@ def main_task():
         if (puck["到达日期"] == '2018-01-19' and puck["出发日期"] == '2018-01-20'):
             puck["优先级"] = 1
         elif (puck["到达日期"] == '2018-01-20' and puck["出发日期"] == '2018-01-20'):
-            puck["优先级"] = 2
+            puck["优先级"] = 2 
         elif (puck["到达日期"] == '2018-01-20' and puck["出发日期"] == '2018-01-21'):
             puck["优先级"] = 3
         else:
@@ -142,9 +142,9 @@ def main_task():
         elif puck["飞机型号"] in NARROW_BODY:
             puck["机体类别"] = "N"
         else:
-            print("error")
+            print("Error")
 
-    # 根据优先级排序
+    # 根据优先级对飞机排序
     tmp_list = []
     first_count = 1
     for puck in list_pucks:
@@ -157,7 +157,7 @@ def main_task():
             tmp_list.insert(first_count, puck)
     list_pucks = tmp_list
     
-    # 3. 同一优先级中采用FIFO, 冒泡排序, 获取飞机分配优先队列
+    # 3. 同一优先级中采用FIFO, 冒泡排序, 获取最终飞机分配优先队列
     for i in range(len(list_pucks) - 1):
         for j in range(len(list_pucks) - i - 1):
             if list_pucks[j]["到达时刻"] > list_pucks[j+1]["到达时刻"] and list_pucks[j]["优先级"] == list_pucks[j+1]["优先级"]:
@@ -177,62 +177,90 @@ def main_task():
         puck["是否分配"] = 0
         resource_period = __time_transfer(puck["到达时刻"], puck["出发时刻"], puck["优先级"])
         for gate in list_gates:
-            # 判断飞机型号 / 到达和出发类型是否匹配
-            if (puck["机体类别"]!= gate["机体类别"]
-             or puck["到达类型"] not in gate["到达类型"]
-             or puck["出发类型"] not in gate["出发类型"]):
+            
+            # 判断飞机型号 / 到达和出发类型是否 匹配
+            if (puck["机体类别"].strip() != gate["机体类别"].strip()
+             or puck["到达类型"].strip() not in gate["到达类型"]
+             or puck["出发类型"].strip() not in gate["出发类型"]):
                 continue
 
             # 判断时间是否冲突
             time_conflict = False
-            for i in range(resource_period["begin_index"], resource_period["end_index"]):
+            for i in range(resource_period["begin_index"], resource_period["end_index"]+1):
                 if gate["资源数组"][i] != 0:
-                    # print("资源已被占用！")
+                    # print("时间资源已被占用！")
                     time_conflict = True
                     break
             if time_conflict:
                 continue
 
-            for i in range(resource_period["begin_index"], resource_period["end_index"]):
+            print("%s %s %s" % (puck["飞机转场记录号"], resource_period["begin_index"], resource_period["end_index"]))
+            for i in range(resource_period["begin_index"], resource_period["end_index"]+1):
                 puck["是否分配"] = 1
                 gate["资源数组"][i] = 1
-
+                puck["对应登机口"] = gate["登机口"]
+            
             # 间隔延迟45分钟 9个break
             if (resource_period["end_index"] < TIME_STEPS - 9):
-                gate["资源数组"][resource_period["end_index"]+1] = 1
-                gate["资源数组"][resource_period["end_index"]+2] = 1
-                gate["资源数组"][resource_period["end_index"]+3] = 1
-                gate["资源数组"][resource_period["end_index"]+4] = 1
-                gate["资源数组"][resource_period["end_index"]+5] = 1
-                gate["资源数组"][resource_period["end_index"]+6] = 1
-                gate["资源数组"][resource_period["end_index"]+7] = 1
-                gate["资源数组"][resource_period["end_index"]+8] = 1
-                gate["资源数组"][resource_period["end_index"]+9] = 1
+                for i in range(1,10):
+                    gate["资源数组"][resource_period["end_index"]+i] = 1
             else:
-                gate["资源数组"][TIME_STEPS-1] = 1
-                gate["资源数组"][TIME_STEPS-2] = 1
-                gate["资源数组"][TIME_STEPS-3] = 1
-                gate["资源数组"][TIME_STEPS-4] = 1
-                gate["资源数组"][TIME_STEPS-5] = 1
-                gate["资源数组"][TIME_STEPS-6] = 1
-                gate["资源数组"][TIME_STEPS-7] = 1
-                gate["资源数组"][TIME_STEPS-8] = 1
-                gate["资源数组"][TIME_STEPS-9] = 1
+                for i in range(1,10):
+                    gate["资源数组"][TIME_STEPS-i] = 1
+            
+            # 分配完毕，下一个puck
+            if puck["是否分配"] == 1:
+                break
 
     # 目标函数
     num_satisfy_airline = 0
     num_free_gate = 0
 
+    gate_resource_narrow = []
+    gate_resource_wide = []
+    list_free_gate = []
     for gate in list_gates:
-        if sum(gate["资源数组"]) == 0 :
+        # print(gate)
+        if(gate["机体类别"] == "N"):
+            gate_resource_narrow.append(gate["资源数组"])
+        if(gate["机体类别"] == "W"):
+            gate_resource_wide.append(gate["资源数组"])
+        if sum(gate["资源数组"]) == 0:
             num_free_gate = num_free_gate + 1
+            list_free_gate.append(gate)
+
     
+    list_satisfy_airline = []
     for puck in list_pucks:
         if puck["是否分配"] == 1:
+            list_satisfy_airline.append(puck)
+            # 每架飞机匹配两个航班
             num_satisfy_airline = num_satisfy_airline + 2
+            
+    # fig = plt.figure(figsize=(20, 4))
+    # ax = fig.add_subplot(221)
+    # plt.imshow(gate_resource_wide)
+    # ax.text(1, 2, 'Wide', fontsize=12, color='r')
+    # cbar = plt.colorbar(plt.imshow(gate_resource_wide), orientation='horizontal')
+
+    # ax = fig.add_subplot(222)
+    # plt.imshow(gate_resource_narrow)
+    # ax.text(1, 2, 'Narrow', fontsize=12, color='r')
+
+    # # plt.title('Resource Ratio', fontsize=12)
+    # cbar = plt.colorbar(plt.imshow(gate_resource_narrow), orientation='horizontal')
+    # cbar.set_label('0-1',fontsize=12)
+    # pyplot.show()
 
     print("num_satisfy_airline : %s " % num_satisfy_airline)
+    # for satisfy_airline in list_satisfy_airline:
+    #     print(satisfy_airline)
+
+    print("---")
     print("num_free_gate : %s " % num_free_gate)
+    # for free_gate in list_free_gate:
+    #     print(free_gate)
+
 
     # 5. 优化
 
